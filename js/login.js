bf8155773db8b481c05addc16fdd2b8083d6d95d
erocksom47@gmail.com <eroksom47@gmail.com>
2026-09-22 19:44:57 -0600

    aplicacion de la utileria  al login

--- js/login.js
+++ js/login.js
@@ -1,9 +1,20 @@
 function iniciar_cesion(){
     let correo = document.getElementById("correo").value;
-    let contraseña = document.getElementById("password").value;
-    iniciar_cesion(correo,contraseña);
-}
+    let contraseña = document.getElementById("contraseña").value;
 
-function validar_correo(){
+    if(validarCorreo(correo)){
+                document.getElementById("email-r").textContent = "Correo válido";
+                document.getElementById("email-r").style.color = "green";
+            }else{
+                document.getElementById("email-r").textContent = "Correo inválido";
+                document.getElementById("email-r").style.color = "red";
+            }
 
+            if(validarPassword(contraseña)){
+                document.getElementById("contraseña-r").textContent = "Contraseña válida";
+                document.getElementById("contraseña-r").style.color = "green";
+            }else{
+                document.getElementById("contraseña-r").textContent = "mínimo 8 caracteres, mayúscula, minúscula,  número y  carácter especial.";
+                document.getElementById("contraseña-r").style.color = "red";
+            }
 }
--- js/utileria.js
+++ js/utileria.js
@@ -1,6 +1,6 @@
-function validar_correo(correo){
+function validarCorreo(c) {
     let correo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return correo.test(correo);
+    return correo.test(c);
 }
 
 function solo_letras(palabra){
@@ -12,8 +12,8 @@ function validarLongitud(entrada,longitud){
     return n.toString().length === longitud;
 }
 
-function calcularEdad(fechaNacimiento){
-    let fechaNacimiento = new Date(fechaNacimiento);
+function calcularEdad(fechaNacimiento_e){
+    let fechaNacimiento = new Date(fechaNacimiento_e);
     let fechaActual = new Date();
 
     let edad= fechaActual.getFullYear()- fechaNacimiento.getFullYear();
@@ -25,4 +25,20 @@ function calcularEdad(fechaNacimiento){
     return edad;
 }
 
+function esMayorEdad(edad){
+    return calcularEdad(edad)>=18;
+}
+
+function validarPassword(e_password){
+    let password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#_-])[A-Za-z\d@$!%*?&.#_-]{8,}$/;
+    return password.test(e_password);
+}
+
+function validarCodigoPostal(e_cd_postal){
+    let codigo_Postal = /^\d{5}$/;
+}
+
+function contarCaracteres(e_texto){
+    return e_texto.length;
+}
 
